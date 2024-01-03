cc32c802d827b5c199c81d349a7c15113279ca9c
aprescott <aprescott@pax8.com>
2024-01-03 10:26:44 -0500

    Initial commit

---
+++ src/main/kotlin/net/adamprescott/hellowebservice/HelloWebServiceApplication.kt
@@ -0,0 +1,51 @@
+package net.adamprescott.hellowebservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@SpringBootApplication
+class HelloWebServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<HelloWebServiceApplication>(*args)
+}
+
+@RestController
+class MessageController(val service: MessageService) {
+    @GetMapping("/")
+    fun index() = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
+}
+
+@Table("MESSAGES")
+data class Message(@Id var id: String?, val text: String)
+
+interface MessageRepository : CrudRepository<Message, String>
+
+@Service
+class MessageService(val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
+
+    fun findMessageById(id: String): List<Message> = db.findById(id).toList()
+
+    fun save(message: Message) {
+        db.save(message)
+    }
+
+    fun <T : Any> Optional<out T>.toList(): List<T> =
+        if (isPresent) listOf(get()) else emptyList()
+}
